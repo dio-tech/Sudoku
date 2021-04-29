ee22e97a05af3f8da32b1c52dc0500798772e8a2
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-04-29 21:54:14 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import os
 # UPDATE GITHUB
 # git commit -m "Initial Commmit"
 os.system('git add .')
-os.system('git commit -m "Initial Commmit"')
+os.system('git commit -m "Initial Commit"')
 os.system('git push')
