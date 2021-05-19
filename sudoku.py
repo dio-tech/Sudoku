91fd1e891ffbb97d2733180f4a04f6f60c85ac5e
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-05-19 22:20:39 +0100

    Initial Commit

--- sudoku.py
+++ sudoku.py
@@ -15,9 +15,9 @@ class Spot:
         self.color = (255, 255, 255)
     
     def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.block_size, self.block_size))
+        pygame.draw.rect(win, self.color, (self.x+1, self.y+1, self.block_size-1, self.block_size-1))
 
-def grid(total_rows, width):
+def get_grid(total_rows, width):
     grid = []
     for i in range(total_rows):
         grid.append([])
@@ -33,6 +33,15 @@ def draw_lines(win, total_rows, width):
         pygame.draw.line(win, (0, 0, 0), (i, 0), (i, width))
         pygame.draw.line(win, (0, 0, 0), (0, i), (width, i))
 
+def get_click(pos, rows, width):
+    x, y = pos
+    block_size = width//rows
+
+    row = y // block_size
+    col = x // block_size
+
+    return row, col
+
 def redraw_window(win, width, total_rows, grid):
     win.fill((255, 255, 255))
     draw_lines(win, total_rows, width)
@@ -45,13 +54,20 @@ def main(win, width):
     run = True
     ROWS = 9
 
-    grid = grid(ROWS, width)
+    grid = get_grid(ROWS, width)
 
     while run:
         redraw_window(win, width, ROWS, grid)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    row, col = get_click(pos, width, ROWS)
+                    print(row, col)
+
         pygame.display.update()
 
 main(WIN, WIDTH)
